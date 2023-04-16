6b1337f6547776f6e485e082c65b5d39e2dbbff3
alfredo cruz <arandevs22@gmail.com>
2023-04-16 01:53:30 -0500

    download mp3

--- src/App.jsx
+++ src/App.jsx
@@ -43,18 +43,16 @@ function App() {
   }
 
   const downloadMp3 = (url) => {
-    fetch(url).then(response => response.blob())
-      .then(blob => {
-        const blobUrl = window.URL.createObjectURL(new Blob([blob]))
-        const fileName = url.split('/').pop();
-        const aTag = document.createElement('a');
-        aTag.href = blobUrl;
-        aTag.setAttribute('download', fileName);
-        document.body.appendChild(aTag);
-        aTag.click();
-        aTag.remove();
-      })
+    const fileName = url.split('/').pop();
+    const aTag = document.createElement('a');
+    aTag.href = url;
+    aTag.setAttribute('download', fileName);
+    document.body.appendChild(aTag);
+    aTag.click();
+    aTag.remove();
   }
+
+
 
   const randomBtn = () => {
     setPlaying(true)
