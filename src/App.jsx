0a27af4bebd5f7c3dd0957737114a4c02ba2bd89
alfredo cruz <arandevs22@gmail.com>
2023-04-18 02:00:48 -0500

    production

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { Box, Button, Container, IconButton, LinearProgress, Paper, Stack, Typography } from '@mui/material'
+import { Box, Button, Container, IconButton, LinearProgress, Paper, Stack, Typography, styled } from '@mui/material'
 import { useEffect, useRef, useState } from 'react'
 import tracks from './data/music'
 import { Download, Loop, PauseCircle, PlayCircle, Shuffle, SkipNext, SkipPrevious } from '@mui/icons-material';
+import { Image } from 'mui-image'
 
 function App() {
 
@@ -78,17 +79,24 @@ function App() {
   })
 
 
+  const TimeLinearProgress = styled(LinearProgress)(({ theme }) => ({
+    height: 10,
+    borderRadius: 5,
+    backgroundColor: 'rgba(255, 255, 255, 0.2)'
+  }))
+
+
   return (
     <div>
       <Container className='main'>
 
         <audio onTimeUpdate={(e) => setCurrentTime(e.target.currentTime)} onLoadedMetadata={(e) => setDuration(e.target.duration)} ref={audioPlayer} className='audio-player' autoPlay={true} src={tracks[count].URL} onEnded={randomBtn} loop={isLoop} />
 
-        <Box sx={{ paddingTop: 3 }} mb={3}>
-          <img className='cover' src={`https://aranstorage.blob.core.windows.net/images/${count}.jpg`} alt={`cover ${count}`} />
+        <Box sx={{ paddingTop: 4, width: '90%', margin: 'auto', textAlign: 'center' }} mb={3}>
+          <Image fit='contain' src={`https://aranstorage.blob.core.windows.net/images/${count}.jpg`} bgColor={`${tracks[count].primaryColor}`} easing='ease' />
         </Box>
 
-        <Box mb={1}>
+        <Box mt={3} mb={1}>
           <Typography className='title' variant="h5" color="#fff">
             {tracks[count].title}
           </Typography>
@@ -96,7 +104,7 @@ function App() {
         <Box>
           <Stack className='artist' direction="row" spacing={2}>
             {tracks[count].artists.map((artist) => (
-              <Typography variant='subtitle1' color="gray" key={artist.name}>
+              <Typography variant='subtitle1' color="rgba(255, 255, 255, 0.6)" key={artist.name}>
                 {artist.name}
               </Typography>
             ))}
@@ -104,8 +112,10 @@ function App() {
         </Box>
 
         {count > 0 &&
-          <Box mt={5} sx={{ color: `${tracks[count].primaryColor}` }}>
-            <LinearProgress color='inherit' variant='determinate' value={progress} />
+          <Box mt={3} sx={{ color: `${tracks[count].primaryColor}` }}>
+            <Box mb={1}>
+              <TimeLinearProgress color='inherit' variant='determinate' value={progress} />
+            </Box>
             <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
               <Typography variant="body1" color="#fff">
                 {calculateTime(currentTime)}
@@ -125,18 +135,18 @@ function App() {
               </Button>
             </Stack>
           </Box> :
-          <Box mt={5} mb={3} >
+          <Box mt={1} mb={3}>
             <Box className="controls-btn" >
               <IconButton onClick={toggleLoop}>
-                {isLoop ? <Loop sx={{ fontSize: 30, color: '#d32f86' }} />
-                  : <Loop sx={{ fontSize: 30, color: '#fff' }} />
+                {isLoop ? <Loop sx={{ fontSize: 30, color: '#fff' }} />
+                  : <Loop sx={{ fontSize: 30, color: 'rgba(255, 255, 255, 0.3)' }} />
                 }
               </IconButton>
               <IconButton onClick={randomBtn}>
                 <SkipPrevious sx={{ fontSize: 40, color: '#fff' }} />
               </IconButton>
               <IconButton onClick={playButton}>
-                {isPlaying ? <PauseCircle sx={{ fontSize: 85, color: '#fff' }} /> : <PlayCircle sx={{ fontSize: 85, color: '#fff' }} />}
+                {isPlaying ? <PauseCircle sx={{ fontSize: 70, color: '#fff' }} /> : <PlayCircle sx={{ fontSize: 70, color: '#fff' }} />}
               </IconButton>
               <IconButton onClick={randomBtn}>
                 <SkipNext sx={{ fontSize: 40, color: '#fff' }} />
